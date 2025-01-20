bb723039fe081586f67137b820a53bde24d7f62a
gaudin <agaudin76@gmail.com>
2025-01-20 22:41:34 +0100

    initial commit for my strapi project

---
+++ src/api/avisgoogle/controllers/avisgoogle.ts
@@ -0,0 +1,7 @@
+/**
+ * avisgoogle controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::avisgoogle.avisgoogle');
---
+++ src/api/avisgoogle/services/avisgoogle.ts
@@ -0,0 +1,7 @@
+/**
+ * avisgoogle service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::avisgoogle.avisgoogle');
